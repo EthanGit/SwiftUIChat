f83818e610ccfb27311725289b79933e771a29ea
Ethan Lin <lyc.ethan@gmail.com>
2025-09-05 16:40:56 +0900

    feat: add ListView

---
+++ SwiftUIChat/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  SwiftUIChat
+//
+//  Created by Ethan Lin on 2025/9/5.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
